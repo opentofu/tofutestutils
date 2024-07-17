1defea4d0aa48658db875f307134ce51afe091b6
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2024-07-17 22:47:49 +0200

    Base project setup
    Signed-off-by: Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
    Co-authored-by: James Humphries <james@james-humphries.co.uk>

---
+++ generate.go
@@ -0,0 +1,6 @@
+// Copyright (c) The OpenTofu Authors
+// SPDX-License-Identifier: MPL-2.0
+
+package tofutestutils
+
+//go:generate go run github.com/opentofu/tofutestutils/internal/tools/license-headers/
