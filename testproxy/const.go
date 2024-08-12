fbcb7fc32380e49f5ad17974e462f1215f1ba415
Janos <86970079+janosdebugs@users.noreply.github.com>
2024-08-12 16:32:11 +0200

    HTTP proxy
    Signed-off-by: Janos <86970079+janosdebugs@users.noreply.github.com>

---
+++ testproxy/const.go
@@ -0,0 +1,12 @@
+// Copyright (c) The OpenTofu Authors
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (c) 2023 HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package testproxy
+
+import (
+	"time"
+)
+
+const cleanupTimeout = 5 * time.Minute
